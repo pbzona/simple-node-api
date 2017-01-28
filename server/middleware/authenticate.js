494014164ac1376718bd1b43a32c6f4768e4ae4f
pbzona <phil.b.zona@gmail.com>
2017-01-28 12:34:01 -0500

    Add tests for POST /users/login

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -2,7 +2,7 @@ const {User} = require('./../models/user');
 
 var authenticate = (req, res, next) => {
     var token = req.header('x-auth');
-    User.findbyToken(token).then((user) => {
+    User.findByToken(token).then((user) => {
         if (!user) {
             return Promise.reject();
         }
